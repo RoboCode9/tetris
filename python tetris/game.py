d2dbdcbd00db2a4b3dfb482a5a1e15265f7ec21c
RoboCode9 <ingresshelps@gmail.com>
2024-03-13 15:23:09 -0500

    implemented sound but its commented out

--- python tetris/game.py
+++ python tetris/game.py
@@ -1,6 +1,7 @@
 from grid import Grid
 from blocks import *
 import random
+import pygame
 
 class Game:
     def __init__(self):
@@ -10,6 +11,11 @@ class Game:
         self.next_block = self.get_random_block()
         self.game_over = False
         self.score = 0
+       #self.rotate_sound = pygame.mixer.Sound("Sounds/rotate.ogg")
+       #self.clear_sound = pygame.mixer.Sound("Sounds/clear.ogg")
+         
+       #pygame.mixer.load("Sounds/music.ogg")
+       #pygame.mixer.music.play(-1)
 
     def update_score(self, lines_cleared, move_down_points):
         if lines_cleared == 1:
@@ -50,6 +56,9 @@ class Game:
         self.current_block = self.next_block
         self.next_block = self.get_random_block()
         rows_cleared = self.grid.clear_full_rows()
+        #if rows_cleared > 0:
+         #   self.clear_sound.play()
+          #  self.update_score(rows_cleared, 0)       
         self.update_score(rows_cleared, 0)
         if self.block_fits() == False:
             self.game_over = True
@@ -72,6 +81,8 @@ class Game:
         self.current_block.rotate()
         if self.block_inside() == False or self.block_fits() == False:
             self.current_block.undo_rotation()
+        #else:
+            #self.rotate_sound.play()
 
     def block_inside(self):
         tiles = self.current_block.get_cell_positions()
@@ -82,4 +93,11 @@ class Game:
     
     def draw(self, screen):
         self.grid.draw(screen)
-        self.current_block.draw(screen)
+        self.current_block.draw(screen, 11, 11)
+
+        if self.next_block.id == 3:
+            self.next_block.draw(screen, 255, 290)
+        elif self.next_block.id == 4:
+            self.next_block.draw(screen, 255, 280)
+        else:
+            self.next_block.draw(screen, 270, 270)
