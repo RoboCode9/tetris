53104421266145d12840bbd05b21fee0fb6192f6
RoboCode9 <ingresshelps@gmail.com>
2024-03-13 13:14:24 -0500

    Block controls implemented

--- python tetris/tetris.py
+++ python tetris/tetris.py
@@ -1,6 +1,5 @@
 import pygame,sys
-from grid import Grid
-from blocks import *
+from game import Game
 
 pygame.init()
 dark_red = (127, 44, 44)
@@ -10,19 +9,24 @@ pygame.display.set_caption("Tetris")
 
 clock = pygame.time.Clock()
 
-game_grid = Grid()
-
-block = ZBlock()
+game = Game()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                game.move_left()
+            if event.key == pygame.K_RIGHT:
+                game.move_right()
+            if event.key == pygame.K_DOWN:
+                game.move_down()
+
     #Drawing
     screen.fill(dark_red)
-    game_grid.draw(screen)
-    block.draw(screen)
+    game.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
